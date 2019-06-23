1db9d9b47bb318695e8be8ee193ac85182dc1aec
enkefalos <48687501+enkefalos@users.noreply.github.com>
2019-06-23 23:56:26 +0300

    Update Submarines.py

--- Submarines.py
+++ Submarines.py
@@ -2,7 +2,6 @@ import numpy as np
 from random import randint
 from enum import Enum
 
-
 class Signal(Enum):
     Already_Destroyed = -2
     Already_Hit = -1
@@ -11,114 +10,126 @@ class Signal(Enum):
     Destroyed = 2
     Won = 3
 
-
 class Board:
     """ This class is the board each player gets """
     def __init__(self, board_size: tuple, vessel_numbers: tuple):
+        self.live_vessel_count = 0
         self.board_size = board_size
-        self.board_array = np.array(board_size)
+        self.board_array = np.zeros(board_size, np.int8)
         self.vessels = []
         self.create_vessels(vessel_numbers)
 
     def create_vessels(self, vessel_numbers: tuple):
-        vessel_number = 0
-        for level in vessel_numbers:
-            for vessel in range(1,vessel_numbers[level]):
-                vessel_number += 1
+        
+        for level in range(0,len(vessel_numbers)):
+            for vessel in range(0,vessel_numbers[level]):
+                self.live_vessel_count += 1
                 if level == 0:
-                    new_vessel = Submarine(vessel_number)
+                    new_vessel = Submarine(self.live_vessel_count)
                 if level == 1:
-                    new_vessel = Destroyer(vessel_number)
+                    new_vessel = Destroyer(self.live_vessel_count)
                 if level == 2:
-                    new_vessel = Jet(vessel_number)
+                    new_vessel = Jet(self.live_vessel_count)
                 self.vessels.append(new_vessel)
                 self.place_vessel(new_vessel, level)
         
-        #for submarine in range(1,vessel_numbers(0)):
-        #    vessel_number += 1
-        #    self.vessels.append(Submarine(vessel_number))
-        #for destroyer in range(1,vessel_numbers(1)):
-        #    vessel_number += 1
-        #    self.vessels.append(Destroyer(vessel_number))
-        #for jet in range(1,vessel_numbers(2)):
-        #    vessel_number += 1
-        #    self.vessels.append(Jet(vessel_number))
-        
-        new_vessel = General(vessel_number)
+        new_vessel = General(self.live_vessel_count + 1)
         self.vessels.append(new_vessel)
-        self.place_vessel(new_vessel, randint(0,level))
+        self.place_vessel(new_vessel, randint(0,2))
       
     def place_vessel(self, vessel, level: int):
         success = False
-        while ~success:
-            # add 50% chance to transpose
+        while not success:
+            if randint(0,1):
+                vessel.area = np.transpose(vessel.area)
             x_coordinate = randint(0, self.board_size[0])
-            x_max = x_coordinate + vessel.area.shape[0]-1
+            x_max = x_coordinate + vessel.area.shape[0]
             if x_max >= self.board_size[0]:
-                pass
+                continue
             y_coordinate = randint(0, self.board_size[1])
-            y_max = y_coordinate + vessel.area.shape[1]-1
+            y_max = y_coordinate + vessel.area.shape[1]
             if y_max >= self.board_size[1]:
-                pass
+                continue
             
             sumValues = 0
             for x in range(x_coordinate, x_max):
                 for y in range(y_coordinate, y_max):
-                    sumValues += self.board_array([x, y, level])
+                    sumValues += self.board_array[x, y, level]
                         
             if sumValues == 0:
-                for x in range(0, vessel.area.shape[0]-1):
-                    for y in range(0, vessel.area.shape[1]-1):
-                        self.board_array[x + x_coordinate, y + y_coordinate, level] = vessel.area([x, y]) * vessel.vessel_number
+                for x in range(0, vessel.area.shape[0]):
+                    for y in range(0, vessel.area.shape[1]):
+                        self.board_array[x + x_coordinate, y + y_coordinate, level] = vessel.area[x, y] * vessel.vessel_number
                 success = True
-
+                vessel.placement_coordinates = [x_coordinate, y_coordinate]
 
     def check_if_won(self, coordinates: tuple):
-
-        result = self.board_array([coordinates])
+        result = self.board_array[coordinates]
         if result > 0:
-            signal = self.vessels[result].hit_vessel()
+            hurt_vessel = self.vessels[result-1]
+            signal = hurt_vessel.hit_vessel(coordinates)
             if signal.value < 3:
-                signal = self.update_board(signal, coordinates, result)
-                if signal.value == 3:
+                if (self.update_board(signal, coordinates, hurt_vessel)):
                     return True
             else:
-                print(signal)
                 return True
         else:
-            print(signal)    
+            print(Signal(result)) 
         return False
 
-    def update_board(self, signal, coordinates: tuple, result: int):
+    def update_board(self, signal, coordinates: tuple, hurt_vessel):
+        """
+        Updates board and returns True is game is over (only general left).
+        """
         self.board_array[coordinates] = -signal.value
         if signal is signal.Destroyed:
-            self.vessels.remove(result)
-            self.board_array[self.vessels[result].coordinates] = -2
-            if len(self.vessels) < 2:
-                return Signal.Won
-        return Signal
+            for x in range(0, hurt_vessel.area.shape[0]):
+                for y in range(0, hurt_vessel.area.shape[1]):
+                    self.board_array[x + hurt_vessel.placement_coordinates[0], y + hurt_vessel.placement_coordinates[1], coordinates[2]] = hurt_vessel.area[x, y] * -2
+            self.live_vessel_count -= 1
+            if self.live_vessel_count == 0:
+                print("Entire fleet destroyed.")
+                return True
+        return False
 
-
-class Vessel():
+class Vessel:
     """ Base class """
     def __init__(self, vessel_number: int):
         self.vessel_number = vessel_number
+        self.placement_coordinates = []
         self.area = None
 
-    def __hit_vessel(self):
-        pass
+    def hit_vessel(self):
+        print("HIT!")
+
+    def destroy_vessel(self , vessel_name: str):
+        print(vessel_name, "was destroyed.")
 
 class Submarine(Vessel):
     """ Level 0 Vessel """
     def __init__(self, vessel_number: int):
         super().__init__(vessel_number)
         self.area = np.array([[1, 1, 1]])
-    
+
+    def hit_vessel(self, coordinates):
+        super().hit_vessel()
+        self.destroy_vessel("Submarine " + str(self.vessel_number))
+        return Signal.Destroyed
+
 class Destroyer(Vessel):
     """ Level 1 Vessel """
     def __init__(self, vessel_number: int):
         super().__init__(vessel_number)
-        self.area =np.array([[1], [1], [1], [1]])
+        self.area = np.array([[1], [1], [1], [1]])
+        self.durability = 4
+
+    def hit_vessel(self, coordinates):
+        super().hit_vessel()
+        self.durability -= 1
+        if self.durability == 0:
+            self.destroy_vessel("Destroyer " + str(self.vessel_number))
+            return Signal.Destroyed
+        return Signal.Hit
 
 class Jet(Vessel):
     """ Level 2 Vessel """
@@ -126,13 +137,21 @@ class Jet(Vessel):
         super().__init__(vessel_number)
         self.area = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0], [0, 1, 0]])
 
+    def hit_vessel(self, coordinates):
+        super().hit_vessel()
+        self.destroy_vessel("Jet " + str(self.vessel_number))
+        return Signal.Destroyed
+
 class General(Vessel):
     """ The General """
     def __init__(self, vessel_number: int):
         super().__init__(vessel_number)
-        self.area = np.array([1])
+        self.area = np.array([[1]])
 
-
+    def hit_vessel(self, coordinates):
+        super().hit_vessel()
+        self.destroy_vessel("The general")
+        return Signal.Won
 
 class GameLoop:
     """ The class running the game loop for the Submarines game """
@@ -145,47 +164,59 @@ class GameLoop:
     def start(self):
         self.has_game_ended = False
         self.is_it_player1s_turn = True
-        print("New game has started!\n")
-        print("blabla\n")
-        while ~self.has_game_ended:
+        print("\nA new game has started!")
+        print("At any given time, you can type 'quit' to exit the game,\nor 'show' to see your board.\n")
+        
+        while not self.has_game_ended:
             self.has_game_ended = self.process_input()
-            
-            self.is_it_player1s_turn = ~self.is_it_player1s_turn
+            self.is_it_player1s_turn = not self.is_it_player1s_turn
         
         if self.is_it_player1s_turn:
-            print("Player 2 Wins!")
+            print("Player 2 Wins!\n")
         else:
-            print("Player 1 Wins!")
-        
+            print("Player 1 Wins!\n")
         
     def process_input(self):
         if self.is_it_player1s_turn:
             player_input = input("Player 1, Type in the coordinates you wish to bomb (x,y,z).")
         else:
-            player_input = input("Player 2, Type in the coordinates you wish to bomb (x,y,z).")
+            player_input = input("Player 2, Type in the coordinates0 you wish to bomb (x,y,z).")
         digits_in_input = []
         for char in player_input:
             if char.isdigit():
                 digits_in_input.append(int(char))
         
-        if len(digits_in_input) == 3:
+        if len(digits_in_input) == 3 and \
+                digits_in_input[0] < self.board_size[0] and \
+                digits_in_input[1] < self.board_size[1] and \
+                digits_in_input[2] < self.board_size[2]:
             coordinates = (digits_in_input[0], digits_in_input[1], digits_in_input[2])
             if self.is_it_player1s_turn:
                 has_game_ended = self.board2.check_if_won(coordinates)
-            else:		
-                has_game_ended = self.board2.check_if_won(coordinates)
+            else:       
+                has_game_ended = self.board1.check_if_won(coordinates)
             return has_game_ended
             
         if len(digits_in_input) == 0:
             if player_input == 'show':
-                self.show_board()
+                if self.is_it_player1s_turn:
+                    self.show_board(self.board1)
+                else:
+                    self.show_board(self.board2)
+                self.is_it_player1s_turn =  not self.is_it_player1s_turn
                 return False
             if player_input == 'quit':
                 raise SystemExit()
                 
         print("Input invalid, try again.")
-        self.is_it_player1s_turn =  ~self.is_it_player1s_turn
+        self.is_it_player1s_turn =  not self.is_it_player1s_turn
         return False
 
-    def show_board(self):
-        pass
+    def show_board(self, board: Board):
+            print("Level 0:\n", board.board_array[..., 0], '\n')
+            print("Level 1:\n", board.board_array[..., 1], '\n')
+            print("Level 2:\n", board.board_array[..., 2], '\n')
+
+if __name__ == '__main__':
+    game = GameLoop()
+    game.start()
